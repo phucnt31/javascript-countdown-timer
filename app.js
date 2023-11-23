df4d9c0502fb2f8713609420f7640385482b77cd
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-23 12:07:28 +0700

    Setup get specific date and time

--- app.js
+++ app.js
@@ -21,3 +21,22 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const deadline = document.querySelector(".deadline");
+const giveaway = document.querySelector(".giveaway");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2023, 4, 24, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year}, ${hours}:${minutes}am`;
